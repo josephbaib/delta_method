3cc1cd955e3a75c4800a4756d4a393e82f400f51
0 Байбурин Юсуф Наилович <bayn18a@econ.msu.ru>
2019-11-05 12:16:21 +0300

    new updates

--- GODSAVETHEQUEEN.R
+++ GODSAVETHEQUEEN.R
@@ -9,12 +9,12 @@ x2 <- rnorm(200, 0, 2)
 x3 <- rnorm(300, 0, 3)
 y <- rnorm(400, 0, 4)
 
-gen_data <- function(a) {
+gen_data <- function(alpha1, alpha2, alpha3, alpha4) {
   x1 <- rnorm(444, 0, 1)
   x2 <- rnorm(444, 0, 1)
   x3 <- rnorm(444, 0, 1)
-  y <- runif(1332, 0, 1)
-  data <- data.frame(x1=x1, x2=x2, x3=x3, y=x1+x2+x3)
+
+  data <- data.frame(x1=x1, x2=x2, x3=x3, y=alpha1*x1+alpha2*x2+alpha3*x3+alpha4)
   return(data)
 }
 
