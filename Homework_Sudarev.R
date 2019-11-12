99499d3679d453cb2e04bf5e2412cf537287ed1c
Калашнов Георгий Юрьевич <gykalashnov@econ.msu.ru>
2019-11-12 09:45:24 +0300

    some corrections

--- Homework_Sudarev.R
+++ Homework_Sudarev.R
@@ -1,11 +1,6 @@
-#Sudarev (mainly researcher)
-gen_data <- function(alpha1, alpha2, alpha3, alpha4) {
-  x1 <- rnorm(444, 0, 1)
-  x2 <- rnorm(444, 0, 1)
-  x3 <- rnorm(444, 0, 1)
-  data <- data.frame(x1=x1, x2=x2, x3=x3, y=alpha1*x1+alpha2*x2+alpha3*x3+alpha4)
-  return(data)
-}
+library("pbapply")
+
+source("GODSAVETHEQUEEN.R")
 
 estimate <- function (data) {
   ols <- lm(y ~ x1 + x2 + x3, data = data)
